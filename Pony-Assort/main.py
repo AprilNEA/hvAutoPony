a08fdc5a9826ec1093a4c01e440744c4a33b1511
槐序二三煊 <sukeycz@gmail.com>
2021-01-29 21:41:19 +0800

    整理文件顺序并修复算法

--- Pony-Assort/main.py
+++ Pony-Assort/main.py
@@ -7,7 +7,7 @@ def detect_option(target):
     match_results = []
     choices_id = []
     for tem in template:
-        temp = cv2.imread(tem)
+        temp = cv2.imread(tem, cv2.IMREAD_GRAYSCALE)
         result = cv2.matchTemplate(target, temp, cv2.TM_SQDIFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
         match_results.append([tem, min_val, min_loc])
@@ -18,44 +18,53 @@ def detect_option(target):
 
 def match_answer(id):
     matching_dict = lambda x: {
-        x == 'pony_option/0.jpg': 'RAINBOW DASH',
-        x == 'pony_option/1.jpg': 'RARITY',
-        x == 'pony_option/2.jpg': 'FLUTTERSHY',
-        x == 'pony_option/3.jpg': 'PINKIE PIE',
-        x == 'pony_option/4.jpg': 'APPLEJACK',
-        x == 'pony_option/5.jpg': 'TWILIGHT SPARKLE'
+        x == 'pony_option/20.jpg': 'RD',
+        x == 'pony_option/21.jpg': 'RA',
+        x == 'pony_option/22.jpg': 'FL',
+        x == 'pony_option/23.jpg': 'PP',
+        x == 'pony_option/24.jpg': 'AP',
+        x == 'pony_option/25.jpg': 'TS'
     }
     return matching_dict(id)[True]
 
 def read_csv():
     answer = []
-    answer_csv = csv.reader(open('D:\Github\AutoPony-S\pony-Assort\csv\pony1000.csv', 'r'))
+    answer_csv = csv.reader(open('D:\Github\AutoPony-S\pony-Assort\csv\\answers2.csv', 'r'))
     for i in answer_csv:
         answer.append(i)
     return answer
 
-template = ['pony_option/0.jpg',
-                'pony_option/1.jpg',
-                'pony_option/2.jpg',
-                'pony_option/3.jpg',
-                'pony_option/4.jpg',
-                'pony_option/5.jpg']
+rows = []
+template = ['pony_option/20.jpg',
+                'pony_option/21.jpg',
+                'pony_option/22.jpg',
+                'pony_option/23.jpg',
+                'pony_option/24.jpg',
+                'pony_option/25.jpg']
 answer = read_csv()
 pony_all = 'D:/Github/AutoPony-S/pony-Assort/pony_img/pony1000/'
 for i in range(1, 1001):
-    option = 0
-    if answer[i] == "A":
+    option = 1
+    if answer[i][0] == "A":
+        option = 0
+    elif answer[i][0] == "B":
         option = 1
-    elif answer[i] == "B":
+    elif answer[i][0] == "C":
         option = 2
-    elif answer[i] == "C":
-        option = 3
+    else:
+        print(str(i) + "WRONG!~")
     files = pony_all + str(i) + ".jpg"
     target = plt.imread(files)
     target = target[..., ::-1]  # RGB --> BGR
     target = target[:40, :700]
+    target = cv2.cvtColor(target, cv2.COLOR_RGB2GRAY)
     option_list = detect_option(target)
-    print(match_answer(option_list[option]))
-
-
-
+    row = ["gs://pont1000testus/" + str(i) + ".jpg", match_answer(option_list[option])]
+    if i == 3:
+        print(answer[i][0])
+        print(option_list)
+        print(row)
+    rows.append(row)
+with open(r'D:\Github\AutoPony-S\pony-Assort\resule\pony1000gdk.csv', 'w', newline='')as f:
+    f_csv = csv.writer(f)
+    f_csv.writerows(rows)
