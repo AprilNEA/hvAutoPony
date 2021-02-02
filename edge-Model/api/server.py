5c9e25c11a4b4708a7f179260b8eaa0d6e36cdf4
DargonXUan <37977109+DargonXuan@users.noreply.github.com>
2021-02-02 10:39:59 +0800

    202122

--- edge-Model/api/server.py
+++ edge-Model/api/server.py
@@ -1,19 +1,19 @@
 from autopony import autopony
-
-#接受 Base64 转换成 JPEG
-'''
-from flask import Flask, request, jsonify
+from flask import jsonify
+from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
+@app.route('/api', methods=['Post'])
+def get_tasks():
+    a = request.values.get("a")
+    # 接受 Base64 转换成 JPEG
+    # pony_path = r'D:\Github\AutoPony-S\pony-Assort\pony_img\pony1000\3.jpg'
+    # print(autopony(pony_path) )#输入小马图，输出A\B\C
+    return a
 
-@app.route("/autopony", methods=["Post"])
-def compute():
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5000, debug=True)
 
-    return
-
-app.run(host="127.0.0.1", port=5000, debug=True)
-'''
-pony_path = r'D:\Github\AutoPony-S\pony-Assort\pony_img\pony1000\3.jpg'
-print(autopony(pony_path) )#输入小马图，输出A\B\C
 
