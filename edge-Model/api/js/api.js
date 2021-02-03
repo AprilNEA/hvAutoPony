fe0817019a8daa6a4034eea23bfe707a9142be90
Grandmasters <Grandmasters@shinezhang.top>
2021-02-03 10:42:35 +0800

    sql add

--- edge-Model/api/js/api.js
+++ edge-Model/api/js/api.js
@@ -1,134 +1,123 @@
 // ==UserScript==
-// @name        Xuan Pony 
-// @icon        https://cdn.jsdelivr.net/npm/hvautoattack@0.0.0/assets/Setting.png
-// @run-at      document-end
-// @compatible  Chrome/Chromium + Tampermonkey
-// @compatible  Firefox + Greasemonkey
-// @version     0.1.0
-// @include     https://*.org/ponytest*
-// @include     http://ponytest.sukeycz.com/
-// @include     http*://hentaiverse.org/*
-// @include     http://alt.hentaiverse.org/*
-// @exclude     http*://hentaiverse.org/pages/showequip.php?*
-// @exclude     http://alt.hentaiverse.org/pages/showequip.php?*
-// @exclude     http*://hentaiverse.org/equip/*
-// @grant       GM_addStyle
-// @grant        GM_xmlhttpRequest
-// @grant        GM_download
-// @connect      localhost
-// @connect      127.0.0.1
+// @name         HV Auto Pony
+// @author       mc, Megumin
+// @namespace    mingxiansen
+// @version      1.6.9.dev1
+// @description  Calculate pony riddle automatically
+// @match        http*://hentaiverse.org/*
+// @match        http://alt.hentaiverse.org/*
+// @compatible   Firefox + Greasemonkey
+// @compatible   Chrome/Chromium + Tampermonkey
+// @run-at       document-end
+// @grant        none
 // ==/UserScript==
 
-const API_SERVER = 'http://127.0.0.1:5000/api';
-
-// ====== DO NOT EDIT THIS LINE BELOW ===== //
-
-const showLog = (log) => {
-  let logEl = document.getElementById('pre');
-
-  if (!logEl) {
-    logEl = document.createElement('pre');
-    logEl.id = 'pre';
-    document.body.appendChild(logEl);
-  }
-
-  logEl.textContent = logEl.textContent === '' ? log : `${logEl.textContent}\n${log}`;
+if (getElem('#navbar') || !getElem('#riddlemaster')) {
+  return;
+}
+var host;
+var getLocation = document.location.href;
+if (getLocation.match('https://')) {
+  host = 'https://47.100.63.75';
+} else if (getLocation.match('http://')) {
+  host = 'http://47.100.63.75';
 }
 
-async function imgOnload(img) {
-  
-  showLog('开始获取小马图片 Base64 URL');
-  const canvas = document.createElement('canvas');
-  canvas.id = 'canvas';
-  canvas.width = img.offsetWidth;
-  canvas.height = img.offsetHeight;
-  document.body.appendChild(canvas);
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
+//Redefine append function
+Object.prototype.appends = function(element) {
+  this.insertAdjacentHTML('beforeend', element);
+};
 
-  const base64DataUrl = canvas.toDataURL('image/jpeg', 1.0);
-
-  showLog('小马图片 Base64 URL 获取成功！');
-  showLog('向 API 发送请求');
-/*
-  const resp = (await (await fetch(API_SERVER, {
-    method: 'POST',
-    body: JSON.stringify({
-      base64: base64DataUrl
-    }),
-    cache: 'no-cache',
-    headers: {
-      'content-type': 'application/json'
-    }
-  })).json());
-*/
-  GM_xmlhttpRequest({
-    method: "POST",
-    url: "http://127.0.0.1:5000/api",
-    headers: {
-        "Content-Type": "application/json"
-        },
-    data: JSON.stringify({
-        base64: base64Data
-      }),
-    onload: function(response){
-        const rp=response.responseText
-/*
-        if (rp=="A"||rp=="B"||rp=="C"){
-          gE('#riddleanswer').value=rp
-          gE('#riddleanswer+img').click()
-        }*/
-        //console.log("请求成功");
-        //console.log(response.responseText);
-        console.log(response.responseText);
-  },
-  //if (resp.code === 0 && resp.answer) {
-    showLog(`获取到答案为：${rp}`);
-    //showLog(`服务器用时：${resp.time}`);
-
-    setTimeout(() => {
-      const inputEl = document.getElementById('riddleanswer');
-      if (inputEl) {
-        inputEl.value = rp;
-        document.querySelector('#riddleanswer+img')?.click();
-      }
-    }, 2000);
-  } else {
-    showLog('服务器出错！');
-  }
-}
-
-(async () => {
-  if (document.getElementById('riddlecounter')) {
-    GM_addStyle(`
-    #pre {
-      position: fixed;
-      top: 0;
-      width: 255px;
-      text-align: left;
-      padding: 10px;
-      background: rgba(1,1,1,0.08);
-    }
-    #canvas {
-      display: none;
-    }
-    `);
-
-    const img = document.querySelector('#riddlebot > img');
-
-    if (typeof img.complete !== 'undefined' && img.complete) {
-      showLog('检测到小马图片加载完成！');
-      imgOnload(img);
-    } else {
-      console.log(img.complete);
-      const interval = setInterval(() => {
-        console.log(img.complete);
-        if (typeof img.complete !== 'undefined' && img.complete) {
-          clearInterval(interval);
-          showLog('检测到小马图片加载完成！');
-          imgOnload(img);
-        }
-      }, 100);
-    }
+(function() {
+  //Catch pony image
+  //e.g. *hentaiverse.org/riddlemaster?uid=xxxx&v=yyyy
+  var img = getElem('img[src*="hentaiverse.org/riddlemaster?"]');
+  if (img && img.src.length > 0) {
+      getElem('div[id="riddlemid"]').appends('<p>Picture detected, uploading ...</p>');
+      //Get img url
+      var img_src = img.src;
+      var param_str = img_src.split('?')[1];
+      var reg = new RegExp("uid=([a-zA-Z0-9]+)&v=([a-zA-Z0-9]+)");
+      var param = param_str.match(reg);
+      var uid = param[1]
+      var v_code = param[2]
+      console.log(uid);
+      console.log(v_code);
+      //Sending uid and v_code to the server for saving
+      var xhr = new XMLHttpRequest();
+      xhr.open('get', img_src, true);
+      xhr.responseType = 'blob';
+      xhr.onload = function() {
+          if (this.status == 200) {
+              //getElem('div[id="riddlemid"]').append('<p>Get picture successfully</p>')
+              var blob_data = this.response;
+              var fd = new FormData();
+              fd.set('uid', uid);
+              fd.set('v_code', v_code);
+              fd.set('img', blob_data, uid + '-' + v_code + '.jpg');
+              //Post image
+              var send_url = host + '/index.php?act=pony_answer';
+              postImage(send_url, function(response_tfx) {
+                  if (response_tfx.status == 'success') {
+                      if (response_tfx.is_chocie) {
+                          getElem('div[id="riddlemid"]').appends('<p>Got answer :' + response_tfx.answer + '</p>');
+                          var input_text = getElem('input[id="riddleanswer"]');
+                          if (input_text.value == '') { input_text.value = response_tfx.answer }
+                          getElem('div[id="riddlemid"]').appends('<p>Submit automatically after 3 seconds</p>');
+                          setTimeout(function() {
+                              getElem('#riddleanswer+img').click();
+                          }, 2000 + Math.random() * 1000);
+                      } else {
+                          getElem('div[id="riddlemid"]').appends('<p>Cannot detect image, no answer :' + response_tfx.answer + '</p>');
+                      }
+                  } else {
+                      getElem('div[id="riddlemid"]').appends('<p>Get response rejected, error :' + response_tfx.reason + '</p>')
+                  }
+              }, fd);
+          } else {
+              getElem('div[id="riddlemid"]').appends('<p>Failed to get image</p>')
+          }
+      };
+      xhr.send();
   }
 })();
+
+//Get element
+function getElem(ele, mode, parent) {
+  if (typeof ele === 'object') {
+      return ele
+  } else if (mode === undefined && parent === undefined) {
+      return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele)
+  } else if (mode === 'all') {
+      return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele)
+  } else if (typeof mode === 'object' && parent === undefined) {
+      return mode.querySelector(ele)
+  }
+}
+
+//Post
+function postImage(href, func, parm) {
+  var xhr = new window.XMLHttpRequest();
+  xhr.open(parm ? 'POST' : 'GET', href);
+  xhr.onerror = function(xhr, status, error) {
+      getElem('div[id="riddlemid"]').appends('<p>An error occurred while getting the answer, program stopped</p>');
+      getElem('div[id="riddlemid"]').appends(xhr);
+      getElem('div[id="riddlemid"]').appends(status);
+      getElem('div[id="riddlemid"]').appends(error);
+  }
+  xhr.onload = function(e) {
+      if (e.target.status >= 200 && e.target.status < 400 && typeof func === 'function') {
+          var data = e.target.response;
+          data = (typeof data == 'string') ? JSON.parse(data) : data;
+          func(data, e);
+      }
+      xhr = null;
+  }
+  xhr.send(parm);
+}
+
+//Report info in console
+function reportInfo(vars, showType = false) {
+  if (showType === true) console.log(typeof vars);
+  console.log(vars);
+}
--- edge-Model/api/js/xuan.js
+++ edge-Model/api/js/xuan.js
@@ -1,122 +1,149 @@
 // ==UserScript==
-// @name         HV Auto Pony
-// @author       mc, Megumin
-// @namespace    mingxiansen
-// @version      1.6.9.dev1
-// @description  Calculate pony riddle automatically
-// @match        http*://hentaiverse.org/*
-// @match        http*://hentaiverse.org/isekai/*
-// @match        http://alt.hentaiverse.org/*
-// @compatible   Firefox + Greasemonkey
-// @compatible   Chrome/Chromium + Tampermonkey
-// @run-at       document-end
-// @grant        none
+// @name        [HV] Fuck Pony
+// @description Why join the navy if you can be a pirate?
+// @icon        https://cdn.jsdelivr.net/npm/hvautoattack@0.0.0/assets/Setting.png
+// @run-at      document-end
+// @compatible  Chrome/Chromium + Tampermonkey
+// @compatible  Firefox + Greasemonkey
+// @version     0.0.0
+// @include     https://*.org/ponytest*
+// @include     http*://hentaiverse.org/*
+// @include     http://alt.hentaiverse.org/*
+// @include     https://e-hentai.org/news.php*
+// @exclude     http*://hentaiverse.org/pages/showequip.php?*
+// @exclude     http://alt.hentaiverse.org/pages/showequip.php?*
+// @exclude     http*://hentaiverse.org/equip/*
+// @grant       GM_addStyle
+// @grant       unsafeWindow
+// @grant       GM_xmlhttpRequest
+// @grant       GM_download
+// @run-at      document-end
+// @connect     localhost
+// @connect     127.0.0.1
+// @connect     172.81.247.39
+//
 // ==/UserScript==
 
-if (getElem('#navbar') || !getElem('#riddlemaster')) {
-    return;
-}
-var host;
-var getLocation = document.location.href;
-if (getLocation.match('https://')) {
-    host = 'https://47.100.63.75';
-} else if (getLocation.match('http://')) {
-    host = 'http://47.100.63.75';
-}
+const API_SERVER = '';
 
-//Redefine append function
-Object.prototype.appends = function(element) {
-    this.insertAdjacentHTML('beforeend', element);
-};
+// ====== DO NOT EDIT THIS LINE BELOW ===== //
 
-(function() {
-    //Catch pony image
-    //e.g. *hentaiverse.org/riddlemaster?uid=xxxx&v=yyyy
-    var img = getElem('img[src*="hentaiverse.org/isekai/riddlemaster?"]');
-    if (img && img.src.length > 0) {
-        getElem('div[id="riddlemid"]').appends('<p>Picture detected, uploading ...</p>');
-        //Get img url
-        var img_src = img.src;
-        var param_str = img_src.split('?')[1];
-        var reg = new RegExp("uid=([a-zA-Z0-9]+)&v=([a-zA-Z0-9]+)");
-        var param = param_str.match(reg);
-        var uid = param[1]
-        var v_code = param[2]
-        //Sending uid and v_code to the server for saving
-        var xhr = new XMLHttpRequest();
-        xhr.open('get', img_src, true);
-        xhr.responseType = 'blob';
-        xhr.onload = function() {
-            if (this.status == 200) {
-                //getElem('div[id="riddlemid"]').append('<p>Get picture successfully</p>')
-                var blob_data = this.response;
-                var fd = new FormData();
-                fd.set('uid', uid);
-                fd.set('v_code', v_code);
-                fd.set('img', blob_data, uid + '-' + v_code + '.jpg');
-                //Post image
-                var send_url = host + '/index.php?act=pony_answer';
-                postImage(send_url, function(response_tfx) {
-                    if (response_tfx.status == 'success') {
-                        if (response_tfx.is_chocie) {
-                            getElem('div[id="riddlemid"]').appends('<p>Got answer :' + response_tfx.answer + '</p>');
-                            var input_text = getElem('input[id="riddleanswer"]');
-                            if (input_text.value == '') { input_text.value = response_tfx.answer }
-                            getElem('div[id="riddlemid"]').appends('<p>Submit automatically after 3 seconds</p>');
-                            setTimeout(function() {
-                                getElem('#riddleanswer+img').click();
-                            }, 2000 + Math.random() * 1000);
-                        } else {
-                            getElem('div[id="riddlemid"]').appends('<p>Cannot detect image, no answer :' + response_tfx.answer + '</p>');
-                        }
-                    } else {
-                        getElem('div[id="riddlemid"]').appends('<p>Get response rejected, error :' + response_tfx.reason + '</p>')
-                    }
-                }, fd);
-            } else {
-                getElem('div[id="riddlemid"]').appends('<p>Failed to get image</p>')
-            }
-        };
-        xhr.send();
-    }
-})();
+const showLog = (log) => {
+  let logEl = document.getElementById('pre');
 
-//Get element
-function getElem(ele, mode, parent) {
-    if (typeof ele === 'object') {
-        return ele
-    } else if (mode === undefined && parent === undefined) {
-        return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele)
-    } else if (mode === 'all') {
-        return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele)
-    } else if (typeof mode === 'object' && parent === undefined) {
-        return mode.querySelector(ele)
-    }
+  if (!logEl) {
+    logEl = document.createElement('pre');
+    logEl.id = 'pre';
+    document.body.appendChild(logEl);
+  }
+
+  logEl.textContent = logEl.textContent === '' ? log : `${logEl.textContent}\n${log}`;
 }
 
-//Post
-function postImage(href, func, parm) {
-    var xhr = new window.XMLHttpRequest();
-    xhr.open(parm ? 'POST' : 'GET', href);
-    xhr.onerror = function(xhr, status, error) {
-        getElem('div[id="riddlemid"]').appends('<p>An error occurred while getting the answer, program stopped</p>');
-        getElem('div[id="riddlemid"]').appends(xhr);
-        getElem('div[id="riddlemid"]').appends(status);
-        getElem('div[id="riddlemid"]').appends(error);
-    }
-    xhr.onload = function(e) {
-        if (e.target.status >= 200 && e.target.status < 400 && typeof func === 'function') {
-            var data = e.target.response;
-            data = (typeof data == 'string') ? JSON.parse(data) : data;
-            func(data, e);
+async function imgOnload(img) {
+  showLog('开始获取小马图片 Base64 URL');
+  const canvas = document.createElement('canvas');
+  canvas.id = 'canvas';
+  canvas.width = img.offsetWidth;
+  canvas.height = img.offsetHeight;
+  document.body.appendChild(canvas);
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0);
+  
+  var img = gE('img[src*="hentaiverse.org/riddlemaster?"]');
+  var img_src = img.src;
+  var param_str = img_src.split('?')[1];
+  var reg = new RegExp("uid=([a-zA-Z0-9]+)&v=([a-zA-Z0-9]+)");
+  var param = param_str.match(reg);
+  var uid = param[1]
+  var v_code = param[2]
+
+  //fd.set('img', blob_data, uid + '-' + v_code + '.jpg');
+
+  const base64DataUrl = canvas.toDataURL('image/jpeg', 1.0);
+  const base64Data = base64DataUrl.slice(23)
+  console.log(base64Data);
+
+  showLog('小马图片 Base64 URL 获取成功！');
+  showLog('向 API 发送请求');
+
+  GM_xmlhttpRequest({
+    method: "POST",
+    url: "http://172.81.247.39/pony/api",
+    headers: {
+        "Content-Type": "application/json"
+        },
+    data: JSON.stringify({
+        base64: base64Data,
+        uid: uid,
+        v_code: v_code
+      }),
+    onload: function(response){
+        console.log(response.responseText);
+        console.log(response);
+        var back = JSON.parse(response.responseText);
+        back = back.return[0]
+        const rp = back.answer
+        console.log(back);
+        console.log(rp);
+        if (rp=="A"||rp=="B"||rp=="C"){
+            gE('#riddleanswer').value=rp
+            gE('#riddleanswer+img').click()
+        }else {
+            showLog('服务器出错');
         }
-        xhr = null;
-    }
-    xhr.send(parm);
+        showLog(`获取到答案为：${back.answer}`);
+        showLog(`服务器用时：${back.time}`);
+
+    },
+  });
 }
 
-//Report info in console
-function reportInfo(vars, showType = false) {
-    if (showType === true) console.log(typeof vars);
-    console.log(vars);
+function gE (ele, mode, parent) {
+    if (typeof ele === 'object') {
+      return ele
+    } else if (mode === undefined && parent === undefined) {
+      return (isNaN(ele * 1)) ? document.querySelector(ele) : document.getElementById(ele)
+    } else if (mode === 'all') {
+      return (parent === undefined) ? document.querySelectorAll(ele) : parent.querySelectorAll(ele)
+    } else if (typeof mode === 'object' && parent === undefined) {
+      return mode.querySelector(ele)
+    }
 }
+
+(async () => {
+  if (document.getElementById('riddlecounter')) {
+    GM_addStyle(`
+    #pre {
+      position: fixed;
+      top: 0;
+      width: 255px;
+      text-align: left;
+      padding: 10px;
+      background: rgba(1,1,1,0.08);
+    }
+    #canvas {
+      display: none;
+    }
+    `);
+
+    const img = document.querySelector('#riddlebot > img');
+
+    setTimeout(() => {
+      if (typeof img.complete !== 'undefined' && img.complete) {
+        showLog('检测到小马图片加载完成！');
+        imgOnload(img);
+      } else {
+        console.log(img.complete);
+        const interval = setInterval(() => {
+          console.log(img.complete);
+          if (typeof img.complete !== 'undefined' && img.complete) {
+            clearInterval(interval);
+            showLog('检测到小马图片加载完成！');
+            imgOnload(img);
+          }
+        }, 100);
+      }
+    }, 5000)
+  }
+})();
