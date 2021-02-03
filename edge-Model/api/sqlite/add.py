742d8575f5b5fc2ce18dc17489dfe7d5fe9a8425
Grandmasters <Grandmasters@shinezhang.top>
2021-02-03 10:42:35 +0800

    sql add

---
+++ edge-Model/api/sqlite/add.py
@@ -0,0 +1,10 @@
+import sqlite3
+def add(uid,name,password,genre,time,timesleft,timesall):
+    conn = sqlite3.connect('pony.db')
+    c = conn.cursor()
+    sql = "INSERT INTO USERPONY (UID,NAME,PASS,GENRE,TIME,TIMESLEFT,TIMESALL) \
+          VALUES ("+uid+", '"+name+"','"+password+"','"+genre+"','"+time+"',"+timesleft+","+timesall+")"
+    c.execute(sql)
+    conn.commit()
+    print('Records created successfully')
+    conn.close()
