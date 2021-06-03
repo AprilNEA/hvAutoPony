5e2b7ff7b06032813bf9a1af2eeb2f20adaccd6d
sukeycz <ubuntu@localhost.localdomain>
2021-06-04 00:42:40 +0800

    refactor: 重构文件布局

--- pony-Get/main.py
+++
@@ -1,9 +0,0 @@
-import wget
-
-# 马图下载
-def download_main(number,start,end):
-   for i in range(start, end+1):
-      url = "http://wsi.prave.men/pics/" + str(i) + ".jpeg"
-      out = '/pony100000/' + str(i) + ".jpeg"
-      wget.download(url, out)
-      print(url + " Finished")
