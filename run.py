dd34f6271690a9ba1d532b0d332ccc068c12b4a0
槐序二三煊 <sukeycz@gmail.com>
2021-07-12 17:38:20 +0800

    feat: 生产部署脚本

---
+++ run.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @author: sukeycz
+# @license: AGPL-3.0 License
+# @contact: sukeycz0@gmail.com
+# @software: PyCharm
+# @project : hvAutoPony
+# @file: run.py
+# @time: 2021/7/12 17:37
+# @desc:
+import multiprocessing
+
+# 协程需要此补丁
+# form gevent import monkey
+# monkey.patch_all()
+# 其他非uvicorn需打开此配置
+# worker_class = "gevent"
+
+# ip + port
+bind = "127.0.0.1:9001"
+# 超时时间
+timeout = 40
+
+# 并行工作进程数
+workers = multiprocessing.cpu_count() * 2 + 1
+# 每个进程开启的线程数
+threads = 2
+#服务器中在pending状态的最大连接数 (建议 64-2048)
+backlog = 2048
+
+
+# sync(同步, eventlet(并发), gevent（协程, tornado, gthread，此处使用
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# 客户端同时最大连接数，适用于 gevent eventlet
+worker_connections = 1000
+
+# 以守护进程形式运行：后台运行
+daemon = True
+
+loglevel = 'debug'
+pidfile = 'log/gunicorn.pid'
+accesslog = 'log/gun-access.log'
+errorlog = 'log/gun-error.log'
+
+# reload=true 自动重启
+# chdir = '/path/' 指定它的工作路径
