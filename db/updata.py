670c03c47fee89e0e95d6c951d6d98854f6b5a26
Xuan <37977109+dargonXuan@users.noreply.github.com>
2022-09-02 17:49:55 +0800

    refactor: final tweaks, then archive

---
+++ db/updata.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @author: sukeycz
+# @license: AGPL-3.0 License
+# @contact: sukeycz0@gmail.com
+# @software: PyCharm
+# @project : hvAutoPony
+# @file: updata.py
+# @time: 2021/7/12 18:12
+# @desc:
+from sqlite import *
+user_delete('5098427')
